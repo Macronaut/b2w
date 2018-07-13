70138d625800f73a46455043195922a32bedd154
Marcos Vinicius Dreher Oliveira <marcos.oliveira@trusthub.com.br>
2018-07-13 12:40:01 -0300

    Fixing errors && Setting JSON to load once again

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,5 @@ angular.module("b2w", []).controller('b2w-controller',function b2wController($sc
 
 $.getJSON('http://www.mocky.io/v2/5a5e38f3330000b0261923a5',function(json_data){
   angular.element("[ng-controller=b2w-controller]").scope().JSONData = json_data["profile"]
-  angular.element("[ng-controller=b2w-controller]").scope().$apply();
-  console.log(json_data["profile"]);
+  angular.element("[ng-controller=b2w-controller]").scope().$apply();  
 });
